9440eecb9b748c5a41bc85b46f218ce81a7d44cd
VictorDev <ememevictor08@gmail.com>
2026-06-10 14:23:55 +0100

    destination starts from /

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="">
-      Hello
+    <main className="min-h-screen flex flex-col items-center justify-center bg-linear-to-br from-acc-clr/10 to-bg-clr px-4">
+      <div className="text-center max-w-3xl mx-auto">
+        {/* Logo */}
+        <div className="flex justify-center mb-6">
+          <div className="rounded-full overflow-hidden bg-white w-20 h-20 flex items-center justify-center shadow-md">
+            <Image
+              src="/pettrak_logo.png"
+              alt="Pettraq logo"
+              width={80}
+              height={80}
+              priority
+              className="object-cover"
+            />
+          </div>
+        </div>
+        
+        <h1 className="text-xl md:text-xl lg:text-3xl font-bold text-sec-clr mb-4 pry-ff">
+          Welcome to Pettraq for Veterinary Clinics
+        </h1>
+        <p className="text-md md:text-md lg:text-lg text-gray-500 pry-ff">
+          Your pet health monitoring system for animals
+        </p>
+        
+        <div className="mt-8 pry-ff">
+          <a 
+            href="/login" 
+            className="inline-block px-8 py-3 bg-acc-clr text-pry-clr rounded-lg cursor-pointer pry-ff font-semibold hover:bg-acc-clr/80 transition-all duration-200 shadow-md hover:shadow-lg"
+          >
+            Login
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
