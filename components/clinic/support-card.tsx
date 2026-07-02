f7f68cf225b10e5dca6f84dcad357b42e723604a
VictorDev <ememevictor08@gmail.com>
2026-07-02 01:47:42 +0100

    change support email

--- components/clinic/support-card.tsx
+++ components/clinic/support-card.tsx
@@ -17,11 +17,11 @@ export default function SupportCard() {
                     </div>
                     <p className="text-gray-600 mb-4">Send us an email and we&apos;ll get back to you within 24 hours.</p>
                     <a 
-                        href="mailto:pettrakteam@gmail.com"
+                        href="mailto:usepetark@gmail.com"
                         className="inline-flex items-center gap-2 text-acc-clr font-medium hover:underline"
                     >
                         <Mail className="w-4 h-4" />
-                        pettrakteam@gmail.com
+                        usepetark@gmail.com
                     </a>
                 </div>
 
